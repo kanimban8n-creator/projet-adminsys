1bc12544adc55ab935210583ed9794b51a8f1518
mohamed <mohamed15star@gamil.com>
2026-02-25 21:28:54 +0100

    gestct : Ajout de l'entête

---
+++ gestct.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+################################################################
+#                                                              #
+# Description : Gestion des conteneurs                         #
+#                                                              #
+# Auteur : Mohamed Kanimba Touré                               #
+#                                                              #
+# Date :  25/02/2026                                           #
+#                                                              #
+################################################################
+
+echo "
+
+Option :
+        - -- Create : Lancer des conteneurs
+
+        - -- Drop :   Supprimer les conteneurs
+
+        - -- Infos :  Caracteristiques des conteneurs ( IP, NOM, user)
+
+        - -- Start :  Demarrage des conteneurs
+
+        - -- Ansible: Déploiement arborescence ansible
+
+    "
